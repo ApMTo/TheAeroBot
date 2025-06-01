cfb5a660d86821080bb44e2d479d4fdf5076bdcf
Eduard Sargsyan <157886186+ApMTo@users.noreply.github.com>
2025-06-01 17:30:37 +0400

    Update guessWord.js

--- games/guessWord.js
+++ games/guessWord.js
@@ -9,14 +9,15 @@ let gameTimeout = null;
 let timer = null;
 let selected = false;
 
+let isListenersInitialized = false;
 
-checkGameStatus = () => {
+function checkGameStatus() {
   if (selected) {
     return true;
   }
   resetGuessGame();
   return false;
-};
+}
 
 const startGuessWork = async (bot, chatId, userId, userName) => {
   isCanceled = false;
@@ -43,9 +44,7 @@ const startGuessWork = async (bot, chatId, userId, userName) => {
   bot.sendMessage(
     chatId,
     `🎲 *Игра началась!*\n👤 *Объясняет:* ${currentPlayerName}`,
-    {
-      parse_mode: "Markdown",
-    }
+    { parse_mode: "Markdown" }
   );
 
   bot.sendMessage(chatId, "🔒 Нажми, чтобы увидеть слово!", {
@@ -57,92 +56,94 @@ const startGuessWork = async (bot, chatId, userId, userName) => {
     },
   });
 
-  bot.on("callback_query", (query) => {
-    if (isCanceled || !gameActive) return;
+  if (!isListenersInitialized) {
+    isListenersInitialized = true;
 
-    const chatId = query.message.chat.id;
-    const userId = query.from.id;
+    bot.on("callback_query", (query) => {
+      if (isCanceled || !gameActive) return;
 
-    if (userId !== currentPlayerId) {
-      return bot.answerCallbackQuery(query.id, {
-        text: "❌ Только ведущий может видеть слово!",
-        show_alert: true,
-      });
-    }
+      const chatId = query.message.chat.id;
+      const userId = query.from.id;
 
-    if (query.data === "show_word") {
-      bot.answerCallbackQuery(query.id, {
-        text: `🤫 Твоё слово: ${currentWord}\nОбъясни его, но не называй напрямую!`,
-        show_alert: true,
-      });
-    } else if (query.data === "change_word") {
-      currentWord = words[Math.floor(Math.random() * words.length)];
-      bot.answerCallbackQuery(query.id, {
-        text: "✅ Слово изменено! Твоё новое слово: " + currentWord,
-        show_alert: true,
-      });
-    }
-  });
+      if (userId !== currentPlayerId) {
+        return bot.answerCallbackQuery(query.id, {
+          text: "❌ Только ведущий может видеть слово!",
+          show_alert: true,
+        });
+      }
 
-  bot.on("message", async (msg) => {
-    if (!gameActive || !currentWord || isCanceled) return;
+      if (query.data === "show_word") {
+        bot.answerCallbackQuery(query.id, {
+          text: `🤫 Твоё слово: ${currentWord}\nОбъясни его, но не называй напрямую!`,
+          show_alert: true,
+        });
+      } else if (query.data === "change_word") {
+        currentWord = words[Math.floor(Math.random() * words.length)];
+        bot.answerCallbackQuery(query.id, {
+          text: "✅ Слово изменено! Твоё новое слово: " + currentWord,
+          show_alert: true,
+        });
+      }
+    });
 
-    const chatId = msg.chat.id;
-    const text = msg.text?.toLowerCase();
-    const userId = msg.from.id;
-    const userName = msg.from.first_name;
+    bot.on("message", async (msg) => {
+      if (!gameActive || !currentWord || isCanceled) return;
 
-    const replyText = msg.reply_to_message?.text?.toLowerCase();
+      const chatId = msg.chat.id;
+      const text = msg.text?.toLowerCase();
+      const userId = msg.from.id;
+      const userName = msg.from.first_name;
 
-    if (
-      (text === currentWord.toLowerCase() ||
-        replyText === currentWord.toLowerCase()) &&
-      userId === currentPlayerId
-    ) {
-      resetGuessGame(chatId);
-      return bot.sendMessage(
-        chatId,
-        "🔴 В связи с тем, что ведущий озвучил загаданное слово, игра завершается!"
-      );
-    }
+      const replyText = msg.reply_to_message?.text?.toLowerCase();
 
-    if (
-      text === currentWord.toLowerCase() ||
-      replyText === currentWord.toLowerCase()
-    ) {
-      bot.sendMessage(
-        chatId,
-        `🎉 *${userName} угадал(а) слово!* Это было: *${currentWord}*`,
-        {
-          parse_mode: "Markdown",
-        }
-      );
-      currentPlayerId = userId;
-      currentPlayerName = userName;
-      currentWord = words[Math.floor(Math.random() * words.length)];
-
-      bot.sendMessage(chatId, `🔄 Новый ведущий: *${currentPlayerName}*`, {
-        parse_mode: "Markdown",
-      });
-
-      bot.sendMessage(chatId, "🔒 Нажми, чтобы увидеть слово!", {
-        reply_markup: {
-          inline_keyboard: [
-            [{ text: "👀 Показать слово", callback_data: "show_word" }],
-            [{ text: "🔄 Сменить слово", callback_data: "change_word" }],
-          ],
-        },
-      });
-
-      clearTimeout(timer);
-      timer = setTimeout(() => {
-        bot.sendMessage(chatId, "⏰ Время вышло! Игра обнуляется.");
+      if (
+        (text === currentWord.toLowerCase() ||
+          replyText === currentWord.toLowerCase()) &&
+        userId === currentPlayerId
+      ) {
         resetGuessGame(chatId);
-      }, 300 * 1000);
-    }
-  });
+        return bot.sendMessage(
+          chatId,
+          "🔴 В связи с тем, что ведущий озвучил загаданное слово, игра завершается!"
+        );
+      }
 
- 
+      if (
+        text === currentWord.toLowerCase() ||
+        replyText === currentWord.toLowerCase()
+      ) {
+        bot.sendMessage(
+          chatId,
+          `🎉 *${userName} угадал(а) слово!* Это было: *${currentWord}*`,
+          {
+            parse_mode: "Markdown",
+          }
+        );
+        currentPlayerId = userId;
+        currentPlayerName = userName;
+        currentWord = words[Math.floor(Math.random() * words.length)];
+
+        bot.sendMessage(chatId, `🔄 Новый ведущий: *${currentPlayerName}*`, {
+          parse_mode: "Markdown",
+        });
+
+        bot.sendMessage(chatId, "🔒 Нажми, чтобы увидеть слово!", {
+          reply_markup: {
+            inline_keyboard: [
+              [{ text: "👀 Показать слово", callback_data: "show_word" }],
+              [{ text: "🔄 Сменить слово", callback_data: "change_word" }],
+            ],
+          },
+        });
+
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+          bot.sendMessage(chatId, "⏰ Время вышло! Игра обнуляется.");
+          resetGuessGame(chatId);
+        }, 300 * 1000);
+      }
+    });
+  }
 };
 
 function resetGuessGame(chatId) {
@@ -151,7 +152,6 @@ function resetGuessGame(chatId) {
   currentPlayerName = "";
   gameActive = false;
   isCanceled = true;
-  callbackQueryHandled = false;
   clearTimeout(timer);
   selected = false;
 }
